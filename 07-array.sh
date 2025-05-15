83a762f356a99cfcfff446ce958a9df5d1b3ce62
Suraj Padhi <surajpadhi4546@gmail.com>
2025-05-15 18:47:36 +0530

    Arrays movies

---
+++ 07-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("Court" "HIT3" "Guntur Kaaram" "#SINGLE")
+
+echo "First Movie: ${MOVIES[0]}"
+echo "Third Movie: ${MOVIES[2]}"
+echo "Third Movie: ${MOVIES[5]}"
+
+echo "All Movies: ${MOVIES[@]}"
