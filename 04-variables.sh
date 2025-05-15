a43b43ab9ce68b1c891ce04924dfb3ae7b67ab56
Suraj Padhi <surajpadhi4546@gmail.com>
2025-05-15 18:22:34 +0530

    Converstions using variables

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+person1=$1 # There should be no space before or after shell, if space is there it won't work.
+person2=$2
+
+echo "$person1: Hey $person2, How are you!"
+echo " $person2:: Hello $person1, Iam fine how are you!"
+echo "$person1:: I am fine too, Whatsup!"
+echo "$person2:: Nothing, Just going to Mars now will you join?"
+echo "$person1:: No you can carryon!!!"
