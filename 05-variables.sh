124c948666a44eed43ada8978f77f2b2fe4a8c97
Suraj Padhi <surajpadhi4546@gmail.com>
2025-05-15 18:27:13 +0530

    Converstions using variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,6 @@
 
 echo "Enter your pin number"
 
-read PIN  # Here PIN is variable
+read -s PIN  # Here PIN is variable, By using "-s" after read command pin won't be visible while typing.
 
 echo "Your Number is :$PIN"
