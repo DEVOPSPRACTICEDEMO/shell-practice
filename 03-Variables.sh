0f33a93ecd08e523ad7e30726cd5a3246f95cf9a
Suraj Padhi <surajpadhi4546@gmail.com>
2025-05-15 18:11:43 +0530

    Hello world started

---
+++ 03-Variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+person1=Trump
+person2=Musk
+
+echo "$person1: Hey $person2, How are you!"
+echo " $person2:: Hello $person1, Iam fine how are you!"
+echo "$person1:: I am fine too, Whatsup!"
+echo "$person2:: Nothing, Just going to Mars now will you join?"
+echo "$person1:: No you can carryon!!!"
