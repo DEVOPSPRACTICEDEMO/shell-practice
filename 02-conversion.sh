514685c8c77b3e7494f6d37925de006a629fa42e
Suraj Padhi <surajpadhi4546@gmail.com>
2025-05-15 18:01:58 +0530

    Hello world started

---
+++ 02-conversion.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump: Hey Musk, How are you!"
+echo " Musk:: Hello Trump, Iam fine how are you!"
+echo "Trump:: I am fine too, Whatsup!"
+echo "Musk:: Nothing, Just going to Mars now will you join?"
+echo "Trump:: No you can carryon!!!"
