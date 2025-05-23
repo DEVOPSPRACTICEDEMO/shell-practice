e56ed64a7ab6ff29329b3db04a86f937240dec1f
Suraj Padhi <surajpadhi4546@gmail.com>
2025-05-23 17:00:06 +0530

    Special Vars

---
+++ 10-install_commands.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+USERID=$i(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Error:: Please run the script with root access"
+else
+    echo "You are running with root access"
+fi
+
+dnf install mysql -y
