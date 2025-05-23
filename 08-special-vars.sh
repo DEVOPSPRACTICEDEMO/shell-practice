607c2197a5d37d39b4860e6acbab9545fbe59d98
Suraj Padhi <surajpadhi4546@gmail.com>
2025-05-23 16:37:42 +0530

    Special Vars

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 echo "All Variables Passed: $@"
+echo "Number of variables: $#"
+echo "Scropt Name: $0"
+echo "Current Directory: $PWD"
+echo "User running this script: $USER"
+echo "Home Directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
+
 
